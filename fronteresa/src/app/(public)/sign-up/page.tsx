3f837e3961c37cb1c992c0a784e6b3d019e3fb50
Moises Lopes <moisesbruh5@gmail.com>
2026-01-16 10:58:07 -0300

    fix: ajuste na margin da pag Registro

--- fronteresa/src/app/(public)/sign-up/page.tsx
+++ fronteresa/src/app/(public)/sign-up/page.tsx
@@ -3,7 +3,7 @@ import { SignupForm } from "@/components/signupForm/page";
 export default function Signin() {
   return (
     <div className="flex justify-center flex-col items-center min-h-screen">
-      <div className="mb-8">
+      <div className="my-8">
         <SignupForm />
       </div>
     </div>
