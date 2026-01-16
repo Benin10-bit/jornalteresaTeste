36c30ae70b4bd4c553eabcaa44b708db803aa7d7
Moises Lopes <moisesbruh5@gmail.com>
2026-01-16 15:29:07 -0300

    feat: cores certas no card destaque

--- fronteresa/src/app/(private)/page.tsx
+++ fronteresa/src/app/(private)/page.tsx
@@ -4,12 +4,8 @@ import { Suspense } from "react";
 
 export default function NewsPage() {
   return (
-    <>
-      <img src='Jornal_dark.png'></img>
       <Suspense fallback={<LoadingGrid quantity={6} />}>
         <TestePage />
       </Suspense>
-
-    </>
   );
 }
